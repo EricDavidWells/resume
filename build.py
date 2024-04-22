58d79a7f269518d1521a90e5b4dbe08956fb4f26
eric <ericwellsrobotics@gmail.com>
2024-04-21 22:15:29 -0700

    squashed-2024-04

--- build.py
+++ build.py
@@ -8,4 +8,4 @@ print(SCRIPT_DIR)
 os.chdir(f'{SCRIPT_DIR}/src')
 return_code = subprocess.run(f'yes | cp {SCRIPT_DIR}/vendor/Awesome-CV/examples/awesome-cv.cls {SCRIPT_DIR}/src', shell=True)
 return_code = subprocess.run([f'xelatex -output-directory={SCRIPT_DIR}/build resume.tex'], shell=True) 
-
+return_code = subprocess.run([f'xelatex -output-directory={SCRIPT_DIR}/build coverletter.tex'], shell=True) 
